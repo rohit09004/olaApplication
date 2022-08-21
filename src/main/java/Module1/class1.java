cfee5858da3274e3835fb7b52412c8b14cb9170d
ROHIT <ROHIT@DESKTOP-8SI0GBB>
2022-08-21 20:54:38 +0530

    stag changes

--- src/main/java/Module1/class1.java
+++ src/main/java/Module1/class1.java
@@ -4,6 +4,8 @@ public class class1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-   System.out.println("i am okay and fine");	}
+   System.out.println(" first time");	
+   System.out.println(" second time ");
+	}
 
 }
